47f55f8fc99c7d8805df22f4fe275589b459462f
SunFounder <support@sunfounder.com>
2022-08-31 11:12:46 +0800

    update config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@
 import sphinx_rtd_theme
 
 project = 'SunFounder Super_Kit_V3.0_for_Raspberry_Pi'
-copyright = '2021, SunFounder'
+copyright = '2022, SunFounder'
 author = 'www.sunfounder.com'
 
 
